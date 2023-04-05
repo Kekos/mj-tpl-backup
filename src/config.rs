51e5b2dfc0641978910b503c10fb9a4dffd95093
Christoffer Lindahl <christoffer@kekos.se>
2023-04-05 22:37:38 +0200

    Implemented "ConfigRepository" and "config" subcommands "list", "new" and "edit".

---
+++ src/config.rs
@@ -0,0 +1,138 @@
+use super::CliArgs;
+use crate::config_repository::ConfigRepository;
+use serde_derive::{Deserialize, Serialize};
+use std::io::stdin;
+use std::string::String;
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    accounts: Vec<MjAccount>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct MjAccount {
+    name: String,
+    private: String,
+    public: String,
+}
+
+impl Config {
+    pub fn get_account(&self, name: &str) -> Option<&MjAccount> {
+        self.accounts.iter().find(|x| x.name == name)
+    }
+
+    pub fn get_account_mut(&mut self, name: &str) -> Option<&mut MjAccount> {
+        self.accounts.iter_mut().find(|x| x.name == name)
+    }
+}
+
+fn get_account_name(args: &CliArgs) -> &str {
+    if let None = args.account {
+        return "default";
+    }
+
+    args.account.as_ref().unwrap().as_str()
+}
+
+pub fn list_accounts() {
+    println!("Name         | Private key            | Public key");
+    println!("---------------------------------------------------------------");
+
+    let config_repo = ConfigRepository::read();
+    config_repo.config.accounts.iter().for_each(|account| {
+        println!(
+            "{} | {} | {}",
+            account.name, account.private, account.public
+        )
+    });
+}
+
+pub fn new_account() {
+    println!("Give the account name or leave blank: ");
+    let mut name = try_read_line();
+    if name.is_empty() {
+        name = String::from("default");
+    }
+
+    println!("Enter Mailjet Private key: ");
+    let private_key = try_read_line();
+
+    println!("Enter Mailjet Public key: ");
+    let public_key = try_read_line();
+
+    let mut config_repo = ConfigRepository::read();
+
+    if let Some(_) = config_repo.config.get_account(&name) {
+        println!(
+            "An account with name {} is already defined! Try `edit` subcommand instead.",
+            name
+        );
+
+        return;
+    }
+
+    config_repo.config.accounts.push(MjAccount {
+        name,
+        private: private_key,
+        public: public_key,
+    });
+
+    ConfigRepository::write(&config_repo);
+
+    todo!("connect");
+}
+
+pub fn edit_account(args: &CliArgs) {
+    let mut config_repo = ConfigRepository::read();
+    let account_name = get_account_name(args);
+    let mut account = match config_repo.config.get_account_mut(account_name) {
+        None => {
+            println!("Account with name {} not found!", account_name);
+
+            return;
+        }
+        Some(a) => a,
+    };
+
+    println!("Enter Mailjet Private key: ");
+    let private_key = try_read_line();
+
+    println!("Enter Mailjet Public key: ");
+    let public_key = try_read_line();
+
+    account.private = private_key;
+    account.public = public_key;
+
+    ConfigRepository::write(&config_repo);
+
+    todo!("connect");
+}
+
+pub fn delete_account(args: &CliArgs) {
+    let mut config_repo = ConfigRepository::read();
+    let account_name = get_account_name(args);
+
+    let iter = config_repo.config.accounts.iter_mut();
+    let old_size = iter.len();
+    let new_accounts: Vec<MjAccount> = iter.filter(|a| a.name != account_name).collect();
+
+    if old_size == new_accounts.len() {
+        println!("Account with name {} not found!", account_name);
+
+        return;
+    }
+
+    config_repo.config.accounts = new_accounts;
+
+    ConfigRepository::write(&config_repo);
+}
+
+fn try_read_line() -> String {
+    let mut input_string = String::new();
+
+    stdin()
+        .read_line(&mut input_string)
+        .expect("Could not read line");
+
+    input_string.trim().to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,51 @@
+mod config;
+mod config_repository;
+
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-struct CliArgs {
+pub struct CliArgs {
     #[command(subcommand)]
     command: CliCommands,
+    /// Optional account name
+    #[arg(global(true))]
+    account: Option<String>,
 }
 
 #[derive(Subcommand)]
 enum CliCommands {
     /// Configures this tool
-    Config,
+    Config {
+        #[command(subcommand)]
+        config_command: CliConfigCommands,
+    },
     /// Performs backup
     Backup,
+}
+
+#[derive(Subcommand)]
+enum CliConfigCommands {
+    /// Lists all configured accounts
+    List,
+    /// Configures an new account
+    New,
+    /// Edits an existing account
+    Edit,
+    /// Deletes an account
+    Delete,
 }
 
 fn main() {
     let args = CliArgs::parse();
 
     match args.command {
-        CliCommands::Config => {
-            println!("Config!");
-        }
+        CliCommands::Config { ref config_command } => match config_command {
+            CliConfigCommands::List => config::list_accounts(),
+            CliConfigCommands::New => config::new_account(),
+            CliConfigCommands::Edit => config::edit_account(&args),
+            CliConfigCommands::Delete => config::delete_account(&args),
+        },
         CliCommands::Backup => {
             println!("Backup!");
         }

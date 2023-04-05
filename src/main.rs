81a3994be4e7d3bcbfcf385707ff388ec0ce7bd2
Christoffer Lindahl <christoffer@kekos.se>
2023-04-05 19:55:25 +0200

    Installed clap crate and implemented structures for CLI commands.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct CliArgs {
+    #[command(subcommand)]
+    command: CliCommands,
+}
+
+#[derive(Subcommand)]
+enum CliCommands {
+    /// Configures this tool
+    Config,
+    /// Performs backup
+    Backup,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArgs::parse();
+
+    match args.command {
+        CliCommands::Config => {
+            println!("Config!");
+        }
+        CliCommands::Backup => {
+            println!("Backup!");
+        }
+    };
 }
